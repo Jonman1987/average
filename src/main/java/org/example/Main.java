e6d713e934f4eef172731837cb3f5ab9500c7204
Evgeny Danilov <Jonman@yandex.ru>
2023-09-17 15:46:54 +0700

    Task 4: final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,40 @@
 package org.example;
 
-
 import java.util.Scanner;
 
 public class Main {
-    public static void Input(int[] massiveElements, int count){
+    public static void input(int[] arrayOfElements, int count) {
         int element;
         Scanner scannerNumber = new Scanner(System.in);
-        for (int j = 0; j < count; j++){
+        for (int j = 0; j < count; j++) {
             element = scannerNumber.nextInt();
-            massiveElements[j] = element;
+            arrayOfElements[j] = element;
         }
     }
 
-    public static float Average(int[] massiveElements, int count){
+    public static float average(int[] arrayOfElements, int count) {
         int summa = 0;
         float average = 0;
-        for (int j = 0; j < count; j++){
-            summa += massiveElements[j];
+        for (int j = 0; j < count; j++) {
+            summa += arrayOfElements[j];
         }
-        average = (float)summa/count;
-        return average;
+        return (float) summa / count;
     }
 
-    public static float AverageEvenNumbers(int[] massiveElements, int count){
+    public static float averageEvenNumbers(int[] arryaOfElements, int count) {
         int summa = 0;
-        int iterator = 0;
+        int i = 0;
         float averageEvenNumbers = 0;
         for (int j = 0; j < count; j++) {
-            if (massiveElements[j] % 2 == 0) {
-                summa += massiveElements[j];
-                iterator++;
+            if (arryaOfElements[j] % 2 == 0) {
+                summa += arryaOfElements[j];
+                i++;
             }
         }
-        averageEvenNumbers = (float)summa/iterator;
+        averageEvenNumbers = (float) summa / i;
         return averageEvenNumbers;
     }
+
     public static void main(String[] args) {
         int count;
         Scanner scannerNumber = new Scanner(System.in);
@@ -43,8 +42,8 @@ public class Main {
         count = scannerNumber.nextInt();
         System.out.println("Введите не более " + count + " чисел");
         int[] massiveElements = new int[count];
-        Input(massiveElements, count);
-        System.out.println("Среднее арифметическое вашего ввода = " + Average(massiveElements, count));
-        System.out.println("Среднее арифметическое четных чисел вашего ввода = " + AverageEvenNumbers(massiveElements, count));
+        input(massiveElements, count);
+        System.out.println("Среднее арифметическое вашего ввода = " + average(massiveElements, count));
+        System.out.println("Среднее арифметическое четных чисел вашего ввода = " + averageEvenNumbers(massiveElements, count));
     }
 }
